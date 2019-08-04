39daf40428ad558b80a33cb28d20041bc72afd55
khuslentuguldur <khuslen.cyber@gmail.com>
2019-08-03 21:31:11 -0500

    updated

--- s_association.tf
+++ s_association.tf
@@ -1,6 +1,17 @@
 resource "aws_route_table_association" "a" {
   subnet_id      = "${aws_subnet.public-a.id}"
-    subnet_id      = "${aws_subnet.public-b.id}"
-      subnet_id      = "${aws_subnet.public-c.id}"
+  route_table_id = "${aws_route_table.r.id}"
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = "${aws_subnet.public-b.id}"
+  route_table_id = "${aws_route_table.r.id}"
+}
+
+
+
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = "${aws_subnet.public-c.id}"
   route_table_id = "${aws_route_table.r.id}"
 }
