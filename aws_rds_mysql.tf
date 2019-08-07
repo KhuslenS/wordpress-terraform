fc37d0055a185751a400e32b1481955b9fe504a6
khuslentuguldur <khuslen.cyber@gmail.com>
2019-08-06 22:02:21 -0500

    updated

--- aws_rds_mysql.tf
+++ aws_rds_mysql.tf
@@ -7,6 +7,6 @@ resource "aws_db_instance" "rds" {
   name              = "mydb"
   username          = "chris"
   password          = "Huslentuguldur2602
-  db_subnet_group_name = "[${aws_subnet.private-a}]"
-  security_group_names = "[${aws_security_group.private}]"
+  db_subnet_group_name = "[${aws_subnet.private-a.id}]"
+  vpc_security_group_ids = "[${aws_security_group.private.id}]"
 }
